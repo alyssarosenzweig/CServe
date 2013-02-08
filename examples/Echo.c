be37545752cd62ff3b268245e811992dc551a852
bobbybee Scratcher <j104@rocketmail.com>
2013-02-08 18:28:18 -0500

    Added comments

--- examples/Echo.c
+++ examples/Echo.c
@@ -1,5 +1,6 @@
 #include "../lib/CServe.h"
 
+// HandleRequest gets called whenever a new client is connected. Applications logic goes in here
 void HandleRequest(int clientID, __CSERVE_Session session){
     char buffer[256];
     for(;;){
@@ -12,6 +13,8 @@ void HandleRequest(int clientID, __CSERVE_Session session){
         }
     }
 }
+
+// CreateServerSocket starts a socket at port 8088. If it returns -1, something happened. ServerMainLoop takes in 3 parameters: the server's socket, the maximum number of users the server can accept, and the function to call to handle a client
 
 int main(int argc, char** argv){
     int socket = CreateServerSocket(8088);
